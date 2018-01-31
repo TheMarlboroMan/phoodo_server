6e17f2d75fe902eeb42544ed83743f4d229cf7f5
Dani <marlborometal@gmail.com>
2018-01-31 16:09:53 +0100

    Removed config

--- config/constantes_app.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Constantes_app_phoodo
-{
-	const URL_WEB='http://localhost/phoodo/';
-	const RUTA_SERVER='/opt/lampp/htdocs/phoodo/';
-}
-?>
--- config/constantes_bbdd.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Constantes_bbdd_phoodo
-{
-	const BBDD_HOST='localhost';
-	const BBDD_USER='root';
-	const BBDD_PASS='****';
-	const BBDD_BASE_DATOS='phoodo';
-}
-?>
